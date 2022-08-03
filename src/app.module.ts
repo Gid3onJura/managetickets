7ff656ad0da0910d9646706adface604132d56aa
Gid3onJura <homebasekorlash@googlemail.com>
2022-08-03 16:15:22 +0200

    delete auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 //import { AppController } from './app.controller';
 //import { AppService } from './app.service';
 import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [AuthModule, TicketsModule]
+  imports: [TicketsModule]
 })
 export class AppModule { }
